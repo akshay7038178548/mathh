ffebb6f81e4dd67fd4ce73f47754e9f742c885ba
Igor Karatayev <igor.cwer@gmail.com>
2013-08-22 22:51:21 +0300

    removed tests and dummy from build.

--- premake4.lua
+++ premake4.lua
@@ -2,8 +2,9 @@
       location "_build"
       kind "StaticLib"
       language "C++"
-      includedirs { "." }
+      includedirs { ".", "./src/glm" }
       files { "src/**.h", "src/**.cpp" }
+      excludes { "src/glm/test/**", "src/glm/glm/core/**.cpp" }
  
       configuration "Debug"
          defines { "DEBUG" }
